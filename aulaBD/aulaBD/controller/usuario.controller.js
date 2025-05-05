32102bb5d3232f914d18be3b66f1829f869f3df3
fernanda <fer.r.benetti@gmail.com>
2025-05-05 11:50:30 -0300

    comecei

--- aulaBD/aulaBD/controller/usuario.controller.js
+++ aulaBD/aulaBD/controller/usuario.controller.js
@@ -6,10 +6,30 @@ exports.listarUsuarios = async function(){
 }
 
 exports.criarUsuario = async function(novo_usuario){
-    if(usuarioRN.validarUsername(novo_usuario.username)){
+    if(usuarioRN.validarUsername(novo_usuario.username) && usuarioRN.validarKeyTamanho(novo_usuario.senha) && usuarioRN.validarKeyMaiuscula(novo_usuario.senha) && usuarioRN.validarKeyNumero(novo_usuario.senha) && usuarioRN.validarKeyCaractereEspecial(novo_usuario.senha)){
         await usuarioDAO.criarUsuario(novo_usuario);
         return true;
     }
     
+    // if(usuarioRN.validarKeyTamanho(novo_usuario.senha)){
+    //     await usuarioDAO.criarUsuario(novo_usuario);
+    //     return true;
+    // }
+
+    // if(usuarioRN.validarKeyMaiuscula(novo_usuario.senha)){
+    //     await usuarioDAO.criarUsuario(novo_usuario);
+    //     return true;
+    // }
+
+    // if(usuarioRN.validarKeyNumero(novo_usuario.senha)){
+    //     await usuarioDAO.criarUsuario(novo_usuario);
+    //     return true;
+    // }
+    
+    // if(usuarioRN.validarKeyCaractereEspecial(novo_usuario.senha)){
+    //     await usuarioDAO.criarUsuario(novo_usuario);
+    //     return true;
+    // }
     return false;
+
 }
--- aulaBD/aulaBD/model/usuario.rn.js
+++ aulaBD/aulaBD/model/usuario.rn.js
@@ -4,3 +4,24 @@ exports.validarUsername = function(username){
         return true;
     return false;
 }
+
+exports.validarKeyTamanho = function(senha){
+    if(senha.length >= 8)
+        return true;
+    return false;
+}
+exports.validarKeyMaiuscula = function(senha){
+    if(/[A-Z]/.test(senha))
+        return true;
+    return false;
+}
+exports.validarKeyNumero = function(senha){
+    if(/\d/.test(senha))
+        return true;
+    return false;
+}
+exports.validarKeyCaractereEspecial = function(senha){
+    if(/[^a-zA-Z0-9]/.test(senha))
+        return true;
+    return false;
+}
